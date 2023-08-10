cb08f8ba03428e0d890b22e865945f558f61cec4
Abdurahman <abdurahman2004@gmail.com>
2023-08-10 16:24:38 -0700

    betty fixes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
-/**
- * This is the main function. It prints a message and returns 0.
- * @return 0 indicating successful execution.
- */
+/* This is the main function. It prints a message.*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
+/* @return 0 indicating successful execution.*/ 
 return (0);
 }
