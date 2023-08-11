5a3fffc7f8a747c588ad4fde9026f9145c84ce5e
Abdurahman <abdurahman2004@gmail.com>
2023-08-11 15:51:26 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - enrty point
+ *
+ * Description: print all lowercase/uppercase letter from a to z and A to Z
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+	char CH = 'A';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	while (CH <= 'Z')
+	{
+		putchar(ch);
+		CH++;
+	}
+	putchar('\n');
+
+	return (0);
+}
