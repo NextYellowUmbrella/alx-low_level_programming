0e5970d5b3c5e925f5d0236bb555bdc82d2db9ab
Abdurahman <abdurahman2004@gmail.com>
2023-08-15 15:56:07 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: print the lowercase elements
+ *
+ * Return: Always 0 (Success)
+*/
+
+viod print_alphabet(void)
+{
+	int n = 97;
+
+	while (n < 123)
+	{
+		_putchar(n);
+		n++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
