e3a2878eeaa912653c6df7b8557bba5dab05cabb
Abdurahman <abdurahman2004@gmail.com>
2023-08-15 14:53:32 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print 0, 1, - 9
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
