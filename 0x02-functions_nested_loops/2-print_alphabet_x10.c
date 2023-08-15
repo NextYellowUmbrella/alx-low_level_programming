26ab533338dd1472d92654c605fa2c157855345a
Abdurahman <abdurahman2004@gmail.com>
2023-08-15 16:57:44 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: print the alphabet ten times
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_alphabet_x10(void)
+{
+	int ch = 97;
+	int count = 0;
+
+	while (count < 10)
+	{
+		_putchar(ch);
+		ch++;
+		if (ch == 123)
+		{
+			count++;
+			ch = 97;
+		}
+	}
+	_putchar('\n');
+
+	return (0);
+}
