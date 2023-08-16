5c9359cde4554ba0abf4e18ef6526967eec79297
Abdurahman <abdurahman2004@gmail.com>
2023-08-15 17:55:09 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha -  states whether input c is part of the alphabet or not
+ *
+ * Return: 1 when 'c' is a leter upper or lowercase otherwise return 0
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90 || c >= 97 && c <= 122);
+		return (1);
+	return (0);
+}
