6900d2c232c4c3645f34349208ae31017a2c98c5
Abdurahman <abdurahman2004@gmail.com>
2023-08-21 15:38:26 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses the string
+ *
+ * @s: string
+ *
+ * Return: Nothing
+*/
+
+void rev_string(char *s)
+{
+	int l, i;
+	char temp;
+
+	for (l = 0; s[l] != '\0', ++1)
+
+	for (i = 0; s[l] != '\0'; i++)
+		;
+
+	for (i = 0; i < l / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[l - 1 - i];
+		s[l - 1 - i] = temp;
+	}
+}
