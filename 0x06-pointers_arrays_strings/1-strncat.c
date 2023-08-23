940b54b196e555c36bf2021bd6af3ea39a2e50a9
Abdurahman <abdurahman2004@gmail.com>
2023-08-23 15:27:51 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strcat - appends
+ *
+ * @dest: first string
+ * @src: second string
+ *
+ * Return: nothing
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int c, i;
+
+	c = 0;
+
+	while (dest[c])
+		c++;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[c + i] = src[i];
+
+	dest[c + i] = '\0';
+
+	return (dest);
+}
