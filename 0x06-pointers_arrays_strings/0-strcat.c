fa9b25464520ee241979528c0db83cd070bbaa2e
Abdurahman <abdurahman2004@gmail.com>
2023-08-23 15:17:12 -0700

    0-strcat.c1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,16 +11,15 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int c;
-	int c2;
+	int c, c2;
 
 	c = 0;
 
 	while (dest[c])
 		c++;
 
-		for (c2 = 0; src(c2); c2++)
-			dest[c++] = src[c2];
+	for (c2 = 0; src(c2); c2++)
+		dest[c++] = src[c2];
 
-		return (dest);
+	return (dest);
 }
