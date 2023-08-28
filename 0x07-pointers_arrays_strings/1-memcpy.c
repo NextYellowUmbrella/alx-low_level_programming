8b25a517d9d0b65c8a7a85baaf673e261de6348d
Abdurahman <abdurahman2004@gmail.com>
2023-08-28 14:23:37 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * *_memcpy - copies items in str to dest
+ * @src: string to copy
+ * @dest: sring location
+ * @n: number of letters to copy
+ * Return: dest
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	for (unsigned int itr = 0; n > 0; itr++; n--)
+	{
+		dest[itr] = src[itr];
+	}
+	return (*dest);
+}
