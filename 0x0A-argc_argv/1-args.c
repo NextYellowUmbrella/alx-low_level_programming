c3f33c47381a8df928206bb1ce53b2b343f7d512
Abdurahman <abdurahman2004@gmail.com>
2023-09-01 16:35:29 -0700

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - hello
+ * @argc: arg
+ * @argv: string
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)*argv[];
+	printf("%d\n", argc);
+	return (0);
+}
